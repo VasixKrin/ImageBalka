2cd19f7048e8a839ddc6b1ad9975786ef01e0f0f
vasix <vasixkrin@gmail.com>
2025-01-22 21:54:29 +0200

    Redefined save method for Image model to slugify title.

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.conf import settings
+from django.utils.text import slugify
+
 
 class Image(models.Model):
     user = models.OneToOneField(
@@ -21,3 +23,8 @@ class Image(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super.save(*args, **kwargs)
