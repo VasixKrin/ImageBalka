eb6ee36aaa18e7ccf51fb44b792a788d3c9eb46c
vasix <vasixkrin@gmail.com>
2025-01-28 18:42:17 +0200

    Added list and detail views for bookmarked images.

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,5 +1,6 @@
 from django.contrib import messages
-from django.http import JsonResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.http import require_POST
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render, get_object_or_404
@@ -57,3 +58,34 @@ def image_like(request):
         except Image.DoesNotExist:
             pass
     return JsonResponse({'status': 'error'})
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 4)
+    page = request.GET.get('page')
+    images_only = request.GET.get('images_only')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        # if page not an integer deliver the first page
+        images = paginator.page(1)
+    except EmptyPage:
+        if images_only:
+            # if AJAX request and page out of range
+            # return an empty page
+            return HttpResponse('')
+        # if page out of range return last page of results
+        images = paginator.page(paginator.num_pages)
+    if images_only:
+        return render(
+            request,
+            'images/image/list_images.html',
+            {'section': 'images', 'images': images}
+        )
+    return render(
+        request,
+        'images/image/list.html',
+        {'section': 'images', 'images': images}
+    )
