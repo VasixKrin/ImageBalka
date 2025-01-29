b2fd89919be661c952d181d66f3a8152d6f98a14
vasix <vasixkrin@gmail.com>
2025-01-29 20:02:13 +0200

    Added ForeignKey limitation to the Action model for target_ct.

--- bookmarks/actions/models.py
+++ bookmarks/actions/models.py
@@ -2,7 +2,19 @@ from django.conf import settings
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from django.db.models import Q
 
+from account.models import Profile, Contact
+from account.apps import AccountConfig
+from images.models import Image
+from images.apps import ImagesConfig
+
+
+CONTENT_TYPE_CHOICES = (
+    Q(app_label=ImagesConfig.name, model=Image.__name__.lower())
+    | Q(app_label=AccountConfig.name, model=Profile.__name__.lower())
+    | Q(app_label=AccountConfig.name, model=Contact.__name__.lower())
+)
 
 
 class Action(models.Model):
@@ -18,7 +30,8 @@ class Action(models.Model):
         blank=True,
         null=True,
         related_name='targe_obj',
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        limit_choices_to=CONTENT_TYPE_CHOICES
     )
     target_id = models.PositiveIntegerField(null=True, blank=True)
     target = GenericForeignKey('target_ct', 'target_id')
