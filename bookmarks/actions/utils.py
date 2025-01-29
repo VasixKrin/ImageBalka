02201fb01ad02b1e8fb0e974277745e273882c01
vasix <vasixkrin@gmail.com>
2025-01-29 20:16:59 +0200

    Updated create_action function to avoid duplicates of user actions in db.

--- bookmarks/actions/utils.py
+++ bookmarks/actions/utils.py
@@ -1,7 +1,25 @@
+import datetime
 from django.contrib.contenttypes.models import ContentType
+from django.utils import timezone
 from .models import Action
 
 
 def create_action(user, verb, target=None):
-   action = Action(user=user, verb=verb, target=target)
-   action.save()
+    now = datetime.now()
+    last_minute = now - datetime.timedelta(seconds=60)
+    similar_actions = Action.objects.filter(
+        user_id=user.id,
+        verb=verb,
+        created__gte=last_minute
+    )
+    if target:
+        target_cd = ContentType.objects.get_for_model(target)
+        similar_actions = similar_actions.filter(
+            target_cd=target_cd,
+            target_id=target.id
+        )
+    if not similar_actions:
+        action = Action(user=user, verb=verb, target=target)
+        action.save()
+        return True
+    return False
